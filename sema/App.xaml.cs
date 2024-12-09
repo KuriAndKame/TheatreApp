38671d6f5d9448494bdbef6bbad70b4168219356
KuriAndKame <defest125@gmail.com>
2024-12-09 13:43:38 +0300

    Добавьте файлы проекта.

---
+++ sema/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace sema
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ sema/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sema
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
