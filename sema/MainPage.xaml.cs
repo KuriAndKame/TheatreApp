495c65481b29eafa7477f0b3b073cf04d49904cc
KuriAndKame <defest125@gmail.com>
2024-12-09 14:49:36 +0300

    Создал главную страницу и класс постановок

--- sema/MainPage.xaml.cs
+++ sema/MainPage.xaml.cs
@@ -1,16 +1,36 @@
-﻿namespace sema
+﻿using System.Collections.ObjectModel;
+
+namespace sema
 {
+    public class Performance
+    {
+        public string? Title { get; set; }
+        public string? Rating { get; set; }
+        public string? Genres { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+
+
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        public ObservableCollection<Performance> Performances { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            Performances = new ObservableCollection<Performance>
+        {
+            new Performance { Title = "Приключение Алисы в стране чудес", Rating = "5.0", Genres = "Комедия", ImageUrl = "alice.jpg" },
+            new Performance { Title = "Подводная лодка", Rating = "5.0", Genres = "Драма", ImageUrl = "das_boot.jpg" }
+        };
+            PerformancesList.ItemsSource = Performances; // Привязка коллекции к CollectionView
         }
+
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            /*
             count++;
 
             if (count == 1)
@@ -19,6 +39,7 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+            */
         }
     }
 
